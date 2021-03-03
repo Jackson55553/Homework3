421f20a35c76d2a03f02222a405d94c4c8b45629
Jackson55553 <79320339+Jackson55553@users.noreply.github.com>
2021-03-03 19:34:33 +0300

    Add files via upload
    законченная игра

--- ThirdLesson1.java
+++ ThirdLesson1.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class ThirdLesson1 {
     public static void main(String[] args) {
         findWord();
+        for (int i = 1; i > 0; i++) {
+            System.out.println("Повторить игру ещё раз? 1 - да/0 - нет");
+            Scanner sc = new Scanner(System.in);
+            int askForReplay = sc.nextInt();
+            if (askForReplay == 1) {
+                findWord();
+            }if (askForReplay == 0){
+                System.out.println("Game over");
+                break;
+            }
+        }
     }
 
     public static void findWord() {
@@ -13,27 +24,33 @@ public class ThirdLesson1 {
             String word = words[s]; //выбрал слово из массива
             Scanner sc = new Scanner(System.in);
             String playerAnswer = sc.next();//игрок ввел слово
-            System.out.println(playerAnswer);
+            int rightLetters = 0;
             if (word.equals(playerAnswer)) {
                 System.out.println("Победа");
                 break;
             } else {
-                int rightLetters = 0;
-                for (int j = 0; j < playerAnswer.length(); j++) {
-                    char a = word.charAt(j);
-                    char b = playerAnswer.charAt(j);
-                    if (a == b) {
-                        System.out.print(a);
-                        rightLetters++;
+                int j = 0;
+                int symb = 0; //счетчик использованных символов
+                while (playerAnswer.length() > j && word.length() > j){
+                        symb++;
+                        char a = word.charAt(j);
+                        char b = playerAnswer.charAt(j);
+                        if (a == b) {
+                            System.out.print(a);
+                            rightLetters++;
+                        }
+                        if (a != b) {
+                            System.out.print(noLeters[j]);
+                        }
+                        j++;
                     }
-                    if (a != b) {
-                        System.out.print(noLeters[j]);
-                    }
+                for (int k = symb; k < 15; k++) {
+                    System.out.print(noLeters[k]);
                 }
-                System.out.println("Вы отгадали " + rightLetters + " букв. Попробуйте ещё раз.");
+            }
+            System.out.println("Вы отгадали " + rightLetters + " букв. Попробуйте ещё раз.");
             }
             System.out.println();
         }
-    }
     }
 
